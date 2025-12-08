c2bae3b57753f3dd660a62bb7e70717acffd8d5e
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-12-08 07:54:51 -0700

    Dominio de Salas actualizado con los atributos

--- CoupProyecto/src/Dominio/Sala.java
+++ CoupProyecto/src/Dominio/Sala.java
@@ -1,6 +1,28 @@
 package Dominio;
 
-public class Sala {
+import Servidor.UnCliente;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Sala {
+    String nombre;
+    List<UnCliente> integrantes = new ArrayList<>();
+    Boolean enEspera = true;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<UnCliente> getIntegrantes() {
+        return integrantes;
+    }
+
+    public void setIntegrantes(List<UnCliente> integrantes) {
+        this.integrantes = integrantes;
+    }
     
 }
