4c440fda460870423f4641e98369a855dd3b1e10
ExportDark <L22440388@mochis.tecnm.mx>
2025-12-08 07:56:58 -0700

    se agrego clase sesion (con sus respectivos metodos) y se agrego funcionalidad a clase UnCliente

--- CoupProyecto/src/Dominio/Sesion.java
+++ CoupProyecto/src/Dominio/Sesion.java
@@ -1,4 +1,32 @@
 package Dominio;
 public class Sesion {
+    private String nombre;
+    private String contra;
 
+    public Sesion(String nombre, String contra) {
+        this.nombre = nombre;
+        this.contra = contra;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + contra;
+    }
+    
 }
