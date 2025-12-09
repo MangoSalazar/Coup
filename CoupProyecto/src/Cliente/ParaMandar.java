ae0fa83b3ebb0676a840fbd37d9cb6871986c34a
Manguito <l22440193@mochis.tecnm.mx>
2025-12-09 14:02:22 -0700

    hice las pruebitas quiñonez

--- CoupProyecto/src/Cliente/ParaMandar.java
+++ CoupProyecto/src/Cliente/ParaMandar.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 public class ParaMandar implements Runnable{
+    String[] mensajitos = {"hola","12345","ola","prueba mandada"};
+    int n = 0;
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     final DataOutputStream salida ;
     public ParaMandar(Socket s) throws IOException {
@@ -13,16 +15,31 @@ public class ParaMandar implements Runnable{
 
     @Override
     public void run() {
-        while ( true ){
-            String mensaje;
+        mandarMensaje();
+    }
+        public void mandarMensaje() {
+        int nMensaje = 0;
+        while (true) {
+            String mensaje = "";
             try {
+                nMensaje = enviarTest(nMensaje, mensaje);
+                /*
                 mensaje = teclado.readLine();
                 salida.writeUTF(mensaje);
+                */
             } catch (IOException ex) {
+                
             }
 
         }
     }
-
+    public int enviarTest(int nMensaje, String mensaje) throws IOException {
+        while (nMensaje < mensajitos.length) {
+            mensaje = mensajitos[nMensaje];
+            salida.writeUTF(mensaje);
+            nMensaje++;
+        }
+        return nMensaje;
+    }
 }
  
