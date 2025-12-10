5c338a73a6d0a101e5eb730a60d006a22b450928
MangoSalazar <l22440193@mochis.tecnm.mx>
2025-12-09 21:33:19 -0700

    clase sesion actualizada

--- CoupProyecto/src/Dominio/Sesion.java
+++ CoupProyecto/src/Dominio/Sesion.java
@@ -1,11 +1,35 @@
 package Dominio;
 public class Sesion {
+    private int id;
     private String nombre;
     private String contra;
-
+    private int ranking;
+    
     public Sesion(String nombre, String contra) {
         this.nombre = nombre;
         this.contra = contra;
+    }
+    public Sesion(int id,String nombre, String contra,int ranking) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contra = contra;
+        this.ranking = ranking;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
     }
 
     public String getContra() {
