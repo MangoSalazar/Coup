65529a4f1be6a85d3a7f90edd63e518e7f5bad2c
CasaPC <ExportDark69@gmail.com>
2025-12-09 20:42:04 -0700

    se agrego la clase Carta

--- CoupProyecto/src/Coup/Carta.java
+++ CoupProyecto/src/Coup/Carta.java
@@ -1,4 +1,29 @@
 package Coup;
 
 public class Carta {
+
+    private Rol rol;
+    private boolean conVida = true;
+
+    public Carta(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Rol obtenerRol() {
+        return rol;
+    }
+
+    public boolean estaViva() {
+        return conVida;
+    }
+
+    public void matar() {
+        this.conVida = false;
+    }
+
+    @Override
+    public String toString() {
+        return rol +" "+ (conVida?"Viva":"Muerta");
+    }
+    
 }
